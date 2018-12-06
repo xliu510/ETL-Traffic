309acc65732b33f827f0fb69f0a9cfbde316ae01
TaTi <tkhutunishvili@gmail.com>
2018-12-05 17:47:18 -0800

    updated human-trafficking.ipynb and etl-traffick.sql

--- etl-traffick.sql
+++ etl-traffick.sql
@@ -1,11 +1,9 @@
 CREATE DATABASE ETL_Traffic;
 USE ETL_Traffic;
 
-drop table referrals_minor_df_2014;
-drop table referrals_all_df_2014;
-drop table referrals_adult_df_2014;
+# 2014 Data Base
 
-CREATE TABLE referrals_minor_df_2014 (
+CREATE TABLE  if not exists referrals_minor_df_2014 (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    nationality TEXT,
    female_minor INT,
@@ -15,7 +13,7 @@ CREATE TABLE referrals_minor_df_2014 (
 );
 
 
-CREATE TABLE referrals_all_df_2014 (
+CREATE TABLE  if not exists referrals_all_df_2014 (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    nationality TEXT,
    female_all INT,
@@ -24,7 +22,7 @@ CREATE TABLE referrals_all_df_2014 (
    year INT
 );
 
-CREATE TABLE referrals_adult_df_2014 (
+CREATE TABLE  if not exists referrals_adult_df_2014 (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    nationality TEXT,
    female_adult INT,
@@ -33,4 +31,118 @@ CREATE TABLE referrals_adult_df_2014 (
    year INT
 );
 
+# Merge 2014 tables into 1 table
+CREATE TABLE IF NOT EXISTS data_2014 AS SELECT referrals_minor_df_2014.nationality,
+    referrals_minor_df_2014.female_minor,
+    referrals_minor_df_2014.male_minor,
+    referrals_minor_df_2014.total_minor,
+    referrals_adult_df_2014.female_adult,
+    referrals_adult_df_2014.male_adult,
+    referrals_adult_df_2014.total_adult,
+    referrals_all_df_2014.female_all,
+    referrals_all_df_2014.male_all,
+    referrals_all_df_2014.total_all,
+    referrals_all_df_2014.year FROM
+    referrals_all_df_2014,
+    referrals_adult_df_2014,
+    referrals_minor_df_2014
+WHERE
+    referrals_adult_df_2014.nationality = referrals_minor_df_2014.nationality
+        AND referrals_adult_df_2014.nationality = referrals_all_df_2014.nationality;
+        
 
+CREATE TABLE if not exists trafficking (
+	id INT NOT NULL AUTO_INCREMENT,
+    nationality VARCHAR(250),
+    year INT,
+	male_adult INT,
+    female_adult INT,
+	male_minor INT,
+    female_minor INT,
+	male_all INT,
+    female_all INT,
+    total_minor INT,
+    total_adult INT,
+    total_all INT,
+    PRIMARY KEY(id));
+    
+    
+# 2015 Data Base
+
+CREATE TABLE if not exists minor_trafficking_UK_2015 (
+	id INT NOT NULL AUTO_INCREMENT,
+	Year INT,
+	Nationality VARCHAR(250),
+    `Domestic Servitude` INT,
+    `Labour Exploitation` INT,
+    `Organ Harvesting` INT,
+    `Sexual Exploitation` INT,
+    `Unknown Exploitation` INT,
+    `2015 Minor Total` INT,
+	PRIMARY KEY(id));
+
+CREATE TABLE if not exists adult_trafficking_UK_2015 (
+	id INT NOT NULL AUTO_INCREMENT,
+	Year INT,
+	Nationality VARCHAR(250),
+    `Domestic Servitude` INT,
+    `Labour Exploitation` INT,
+    `Organ Harvesting` INT,
+    `Sexual Exploitation` INT,
+    `Unknown Exploitation` INT,
+    `2015 Adult Total` INT,
+	PRIMARY KEY(id));
+    
+CREATE TABLE if not exists all_trafficking_UK_2015 (
+	id INT NOT NULL AUTO_INCREMENT,
+	Year INT,
+	Nationality VARCHAR(250),
+    `Domestic Servitude` INT,
+    `Labour Exploitation` INT,
+    `Organ Harvesting` INT,
+    `Sexual Exploitation` INT,
+    `Unknown Exploitation` INT,
+    `2015 Total` INT,
+	PRIMARY KEY(id));
+    
+
+# 2016 Data Base
+
+
+CREATE TABLE if not exists minor_trafficking_UK_2016 (
+	id INT NOT NULL AUTO_INCREMENT,
+	Year INT,
+	Nationality VARCHAR(250),
+    `Domestic Servitude` INT,
+    `Labour Exploitation` INT,
+    `Organ Harvesting` INT,
+    `Sexual Exploitation` INT,
+    `Unknown Exploitation` INT,
+    `2016 Minor Total` INT,
+	PRIMARY KEY(id));
+
+CREATE TABLE if not exists adult_trafficking_UK_2016 (
+	id INT NOT NULL AUTO_INCREMENT,
+	Year INT,
+	Nationality VARCHAR(250),
+    `Domestic Servitude` INT,
+    `Labour Exploitation` INT,
+    `Organ Harvesting` INT,
+    `Sexual Exploitation` INT,
+    `Unknown Exploitation` INT,
+    `2016 Adult Total` INT,
+	PRIMARY KEY(id));
+    
+CREATE TABLE if not exists all_trafficking_UK_2016 (
+	id INT NOT NULL AUTO_INCREMENT,
+	Year INT,
+	Nationality VARCHAR(250),
+    `Domestic Servitude` INT,
+    `Labour Exploitation` INT,
+    `Organ Harvesting` INT,
+    `Sexual Exploitation` INT,
+    `Unknown Exploitation` INT,
+    `2016 Total` INT,
+	PRIMARY KEY(id));
+    
+    
