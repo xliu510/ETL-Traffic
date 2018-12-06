4d625839821bd40767574d388b3e9abfcaa54a1c
TaTi <tkhutunishvili@gmail.com>
2018-12-06 00:34:49 -0800

    updated etl-traffick.sql and human-trafficking.ipynb, add app.py, static and template folders

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, jsonify, request, render_template
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
+#################################################
+# DB Setup
+#################################################
+
+db = pymysql.connect("localhost", "username", "password", "ETL_Traffic_UK")
+
+#################################################
+# Flask Setup
+#################################################
+
+app = Flask(__name__)
+#api = Api(app)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+@app.route("/data2014")
+def data2014():
+    cursor = db.cursor()
+    sql = "SELECT nationality,total_minor,total_adult,total_all FROM data_2014 ORDER BY CAST(total_all as SIGNED INTEGER)  desc"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return render_template('data2014.html', results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route("/data2015")
+def data2015():
+    cursor = db.cursor()
+    sql = "SELECT * FROM data_2015 ORDER BY CAST(`2015 Total` as SIGNED INTEGER)  desc"
+    cursor.execute(sql)
+    results2015 = cursor.fetchall()
+    return render_template('data2015.html', results2015=results2015)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route("/data2016")
+def data2016():
+    cursor = db.cursor()
+    sql = "SELECT * from data_2016 ORDER BY CAST(`2016 Total` as SIGNED INTEGER)  desc"
+    cursor.execute(sql)
+    results2016 = cursor.fetchall()
+    return render_template('data2016.html', results2016=results2016)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- etl-traffick.sql
+++ etl-traffick.sql
@@ -1,5 +1,6 @@
-CREATE DATABASE ETL_Traffic;
-USE ETL_Traffic;
+
+CREATE DATABASE if not exists ETL_Traffic_UK;
+USE ETL_Traffic_UK;
 
 # 2014 Data Base
 
@@ -51,24 +52,27 @@ WHERE
         AND referrals_adult_df_2014.nationality = referrals_all_df_2014.nationality;
         
 
-CREATE TABLE if not exists trafficking (
-	id INT NOT NULL AUTO_INCREMENT,
-    nationality VARCHAR(250),
-    year INT,
-	male_adult INT,
-    female_adult INT,
-	male_minor INT,
-    female_minor INT,
-	male_all INT,
-    female_all INT,
-    total_minor INT,
-    total_adult INT,
-    total_all INT,
-    PRIMARY KEY(id));
-    
-    
 # 2015 Data Base
 
+
+CREATE TABLE if not exists trafficking_UK_2015 (
+	id INT NOT NULL AUTO_INCREMENT,
+	Year INT,
+	Nationality VARCHAR(250),
+    `Domestic Servitude Minor` INT,
+    `Labour Exploitation Minor` INT,
+    `Organ Harvesting Minor` INT,
+    `Sexual Exploitation Minor` INT,
+    `Unknown Exploitation Minor` INT,
+    `2015 Minor Total` INT,
+	`Domestic Servitude Adult` INT,
+    `Labour Exploitation Adult` INT,
+    `Organ Harvesting Adult` INT,
+    `Sexual Exploitation Adult` INT,
+    `Unknown Exploitation Adult` INT,
+    `2015 Adult Total` INT,
+	PRIMARY KEY(id));
+    
 CREATE TABLE if not exists minor_trafficking_UK_2015 (
 	id INT NOT NULL AUTO_INCREMENT,
 	Year INT,
@@ -105,10 +109,39 @@ CREATE TABLE if not exists all_trafficking_UK_2015 (
     `2015 Total` INT,
 	PRIMARY KEY(id));
     
+# Merge 2015 tables into 1 table  
+	CREATE TABLE IF NOT EXISTS data_2015 AS SELECT minor_trafficking_UK_2015.nationality,
+    minor_trafficking_UK_2015.`2015 Minor Total`,
+    adult_trafficking_UK_2015.`2015 Adult Total`,
+    all_trafficking_UK_2015.`2015 Total`,
+    all_trafficking_UK_2015.year FROM
+    minor_trafficking_UK_2015,
+    adult_trafficking_UK_2015,
+    all_trafficking_UK_2015
+WHERE
+    minor_trafficking_UK_2015.nationality = adult_trafficking_UK_2015.nationality
+        AND minor_trafficking_UK_2015.nationality = all_trafficking_UK_2015.nationality;
+
 
 # 2016 Data Base
 
-
+CREATE TABLE if not exists trafficking_UK_2016 (
+	id INT NOT NULL AUTO_INCREMENT,
+	Year INT,
+	Nationality VARCHAR(250),
+    `Domestic Servitude Minor` INT,
+    `Labour Exploitation Minor` INT,
+    `Sexual Exploitation Minor` INT,
+    `Unknown Exploitation Minor` INT,
+    `2016 Minor Total` INT,
+	`Domestic Servitude Adult` INT,
+    `Labour Exploitation Adult` INT,
+    `Organ Harvesting Adult` INT,
+    `Sexual Exploitation Adult` INT,
+    `Unknown Exploitation Adult` INT,
+    `2016 Adult Total` INT,
+	PRIMARY KEY(id));
+    
 CREATE TABLE if not exists minor_trafficking_UK_2016 (
 	id INT NOT NULL AUTO_INCREMENT,
 	Year INT,
@@ -144,5 +177,17 @@ CREATE TABLE if not exists all_trafficking_UK_2016 (
     `Unknown Exploitation` INT,
     `2016 Total` INT,
 	PRIMARY KEY(id));
+    show tables;
     
-    
+# Merge 2016 tables into 1 table
+    CREATE TABLE IF NOT EXISTS data_2016 AS SELECT minor_trafficking_UK_2016.nationality,
+    minor_trafficking_UK_2016.`2016 Minor Total`,
+    adult_trafficking_UK_2016.`2016 Adult Total`,
+    all_trafficking_UK_2016.`2016 Total`,
+    all_trafficking_UK_2016.year FROM
+    minor_trafficking_UK_2016,
+    adult_trafficking_UK_2016,
+    all_trafficking_UK_2016
+WHERE
+    minor_trafficking_UK_2016.nationality = adult_trafficking_UK_2016.nationality
+        AND minor_trafficking_UK_2016.nationality = all_trafficking_UK_2016.nationality;
